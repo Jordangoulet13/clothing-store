971abb849110c48fbe84a55a85e30fd6777e2ca8
unknown <jordangoulet13@gmail.com>
2021-03-05 13:04:36 -0800

    added redux-persist to root-reducer and store to persist cart state in local storage

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,10 +9,7 @@ import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-import {
-  selectCartHidden,
-  selectCartItemsCount,
-} from "../../redux/cart/cart.selectors";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
 
 import "./header.styles.scss";
